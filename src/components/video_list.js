1f289fc091fbcdfefee6dbb8d581ac2671ddf1c7
Cesar Mejia Leiva <cmejia@gmail.com>
2018-05-28 11:33:17 -0700

    section 02 complete;  added fa icons

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import VideoListItem from './video_list_item'
 
-const VideoList = ({ videos }) => {
+const VideoList = (props) => {
+  const { videos } = props
   return (
     <ul className='col-md-4 list-group'>
-      {videos.map((video) => <VideoListItem key={video.etag} video={video} />)}
+      {videos.map((video) => (
+        <VideoListItem
+          key={video.etag}
+          video={video}
+          onVideoSelect={props.onVideoSelect}
+        />
+      ))}
     </ul>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,36 +1,51 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
+import _ from 'lodash'
 
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyBgxQy0aDqd4m3s4zsXkO-I3Mlv_se0DFk'
-// Create a new component. This component should produce
-// some HTML
 
 class App extends Component {
   constructor (props) {
     super(props)
-    this.state = { videos: [] }
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
 
-    YTSearch({ key: API_KEY, term: 'chemistry' }, (videos) => {
-      this.setState({ videos })
+    this.videoSearch('marvel')
+  }
+
+  videoSearch = (term) => {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
     })
   }
 
   render () {
+    const videoSearch = _.debounce((term) => this.videoSearch(term), 300)
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <div className='nav'>
+          <i className='fab fa-youtube fa-3x' />
+          <div className='nav-heading'>CesarTube</div>
+          <SearchBar onSearchTermChange={videoSearch} />
+        </div>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
+        />
       </div>
     )
   }
 }
-
-// Take this component's generated HTML and put it
-// on the page (in the DOM)
 
 ReactDOM.render(<App />, document.querySelector('.container'))
