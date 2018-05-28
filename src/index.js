b6cf57aa49aa349065e4b8ca2a67282af5486bdb
Cesar Mejia Leiva <cmejia@gmail.com>
2018-05-27 19:29:27 -0700

    setion 02, lesson 28

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyBgxQy0aDqd4m3s4zsXkO-I3Mlv_se0DFk'
 // Create a new component. This component should produce
 // some HTML
-YTSearch({ key: API_KEY, term: 'surfboards' }, (data) => console.log(data))
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { videos: [] }
+
+    YTSearch({ key: API_KEY, term: 'chemistry' }, (videos) => {
+      this.setState({ videos })
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 
 // Take this component's generated HTML and put it
