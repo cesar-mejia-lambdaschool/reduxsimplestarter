209de9dca1c2854255f99f5364075c9cce31f8b3
Cesar Mejia Leiva <cmejia@gmail.com>
2018-06-05 18:52:43 -0700

    bug with fetchWeather

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import fetchWeather from '../actions/index'
 
 class SearchBar extends Component {
   constructor (props) {
@@ -6,6 +9,7 @@ class SearchBar extends Component {
     this.state = {
       term: ''
     }
+    this.onFormSubmit = this.onFormSubmit.bind(this)
   }
 
   onInputChange = (e) => {
@@ -15,8 +19,13 @@ class SearchBar extends Component {
     })
   }
 
-  onFormSubmit = (e) => {
+  onFormSubmit (e) {
     e.preventDefault()
+    console.log(this.props.fetchWeather, 'in formsubmit')
+    this.props.fetchWeather(this.state.term)
+    this.setState({
+      term: ''
+    })
   }
 
   render () {
@@ -25,7 +34,7 @@ class SearchBar extends Component {
         <form className='input-group' onSubmit={this.onFormSubmit}>
           <input
             className='form-control'
-            placeholder='Get a five-day forecast in your favorite cities'
+            placeholder='city, country'
             value={this.state.term}
             onChange={this.onInputChange}
           />
@@ -40,4 +49,8 @@ class SearchBar extends Component {
   }
 }
 
-export default SearchBar
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar)
